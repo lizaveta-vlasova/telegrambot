fb4bfba9e03e55a8edcda7ac3dca69c7a92b168f
Elizaveta Vlasova <elizaveta.vlasova@t-systems.com>
2019-11-24 19:26:33 +0300

    trying to apply proxy

--- src/main/java/telegram/Bot.java
+++ src/main/java/telegram/Bot.java
@@ -4,12 +4,18 @@ import com.annimon.tgbotsmodule.BotHandler;
 import com.annimon.tgbotsmodule.BotModule;
 import com.annimon.tgbotsmodule.Runner;
 import com.annimon.tgbotsmodule.beans.Config;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 import java.util.List;
 
 public class Bot implements BotModule {
 
     public static void main(String[] args) {
+        System.getProperties().put("proxySet", "true");
+        System.getProperties().put("socksProxyHost", "127.0.0.1");
+
+        System.getProperties().put("socksProxyPort", "9150");
+        ApiContextInitializer.init();
         final var profile = (args.length >= 1 && !args[0].isEmpty()) ? args[0] : "";
         Runner.run(profile, List.of(new Bot()));
     }
