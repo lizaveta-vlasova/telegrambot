f0a337ad290f24bdf159180575ee25cec909794c
Elizaveta Vlasova <elizaveta.vlasova@t-systems.com>
2019-11-24 20:49:50 +0300

    new proxy

--- src/main/java/telegram/Bot.java
+++ src/main/java/telegram/Bot.java
@@ -4,7 +4,6 @@ import com.annimon.tgbotsmodule.BotHandler;
 import com.annimon.tgbotsmodule.BotModule;
 import com.annimon.tgbotsmodule.Runner;
 import com.annimon.tgbotsmodule.beans.Config;
-import org.telegram.telegrambots.ApiContextInitializer;
 
 import java.util.List;
 
@@ -13,9 +12,8 @@ public class Bot implements BotModule {
     public static void main(String[] args) {
         System.getProperties().put("proxySet", "true");
         System.getProperties().put("socksProxyHost", "80.211.24.203");
-
         System.getProperties().put("socksProxyPort", "1080");
-        ApiContextInitializer.init();
+
         final var profile = (args.length >= 1 && !args[0].isEmpty()) ? args[0] : "";
         Runner.run(profile, List.of(new Bot()));
     }
